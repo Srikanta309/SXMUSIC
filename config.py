6b9e14a30c7b854f2df62720a001011207eb9c01
Srikanta309 <139033378+Srikanta309@users.noreply.github.com>
2023-12-25 09:31:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,14 +12,13 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # Add Owner Username without @ 
-OWNER_USERNAME = getenv("OWNER_USERNAME","I_am_srikanta")
+OWNER_USERNAME = getenv("OWNER_USERNAME","OfficiallyAgent")
 # Get Your bot username
-BOT_USERNAME = getenv("BOT_USERNAME" , "Srikantaop_bot")
+BOT_USERNAME = getenv("BOT_USERNAME" , "SmMusic03_BOT")
 # Don't Add style font 
-BOT_NAME = getenv("BOT_NAME" , "˹𝐓𝐚𝐫𝐠𝐞𝐭 ✘ 𝐇𝐢𝐭˼")
+BOT_NAME = getenv("BOT_NAME" , "ڳsе𝐥𝗳ịզ𝓾еท")
 #get Your Assistant User name
-ASSUSERNAME = getenv("ASSUSERNAME" , "˹𝐓𝐚𝐫𝐠𝐞𝐭 ✘ 𝐇𝐢𝐭˼")
-
+ASSUSERNAME = getenv("ASSUSERNAME" , "ڳsе𝐥𝗳ịզ𝓾еท")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
@@ -29,7 +28,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 LOGGER_ID = int(getenv("LOGGER_ID", -1001802990747))
 
 # Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 5560295791))
+OWNER_ID = int(getenv("OWNER_ID", 609517172))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -46,8 +45,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/secret_societ")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/secret_societ")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SAZIDBGD1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SAZIDBGD1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
